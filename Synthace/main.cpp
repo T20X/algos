cefcdc31ea2aa86fb5f52cb158bf687564e83b5e
t20x <mpx@gmail.com>
2019-10-23 13:40:38 +0100

    sdf

---
+++ Synthace/main.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <iostream>
+#include <unordered_set>
+#include <unordered_map>
+#include <list>
+#include <memory>
+
+#include "prob1.h"
+#include "prob2.h"
+#include "prob3.h"
+
+int main()
+{
+    prob1::test();
+    prob2::test();
+    prob3::test();
+    return 0;
+}
---
+++ Synthace/prob3.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace prob1
+{
+    static void test()
+    {
+
+    }
+}
+
